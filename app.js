35a8eb85b276a6c7c0b54f26a87c475a303dfa7b
glenturner <gturner0316@yahoo.com>
2021-11-20 19:50:34 -0500

    Quick commit by Glen Turner

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const createGrid = (rows, cols) => {
         cell.setAttribute("id", "select-" + _selectIndex++)
         container.appendChild(cell).className = "grid_item";
         cell.style.background = "#" + randomColor;
-        ScrollReveal().reveal('.grid_item', { opacity: 0.2, viewFactor: 0.5, scale: 0.2, delay: 0, reset: false, distance: '150px', origin: 'left' });
+        ScrollReveal().reveal('.grid_item', { opacity: 0.2, viewFactor: 0.5, scale: 0.1, delay: 200, reset: false, distance: '150px', origin: 'left' });
     };
 };
 
@@ -38,7 +38,6 @@ const onHeightChange = () => {
         }
     });
 }
-
 
 createGrid(12, 12);
 onColorChange()
