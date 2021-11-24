90d3cce63f01b7ddc5df1dc64c4a4da2e0149edf
glenturner <gturner0316@yahoo.com>
2021-11-23 19:59:17 -0500

    Quick commit by Glen Turner

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 let cell;
-let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+let randomColor = () => Math.floor(Math.random() * 16777215).toString(16);
 let _selectIndex = 0;
 
 const createGrid = (rows, cols) => {
     for (c = 0; c < (rows * cols); c++) {
         cell = document.createElement("div");
         cell.setAttribute("id", "select-" + _selectIndex++)
+        cell.style.background = "#" + randomColor();
         container.appendChild(cell).className = "grid_item";
-        cell.style.background = "#" + randomColor;
         ScrollReveal().reveal('.grid_item', { opacity: 0.2, viewFactor: 0.5, scale: 0.1, delay: 100, reset: false, distance: '150px', origin: 'left' });
     };
 };
